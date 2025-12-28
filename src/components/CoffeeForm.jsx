46315874d1304abb0fa87f91b9b7f1898c46b5c9
WJijon223 <williamjijon223@gmail.com>
2025-12-28 15:26:22 -0500

    added options and scroll down to coffeeForm component

--- src/components/CoffeeForm.jsx
+++ src/components/CoffeeForm.jsx
@@ -1,3 +1,5 @@
+import { coffeeOptions } from "../utils";
+
 export default function CoffeeForm() {
   return (
     <>
@@ -5,6 +7,32 @@ export default function CoffeeForm() {
         <i className="fa-solid fa-pencil"></i>
         <h2>Start Tracking Today</h2>
       </div>
+      <h4>Select coffee type</h4>
+      <div className="coffee-grid">
+        {coffeeOptions.slice(0, 5).map((option, index) => {
+          return (
+            <button className="button-card" key={index}>
+              <h4>{option.name}</h4>
+              <p>{option.caffeine}mg</p>
+            </button>
+          );
+        })}
+        <button className="button-card">
+          <h4>Other</h4>
+          <p>N/A</p>
+        </button>
+      </div>
+      {/*  */}
+      <select name="coffee-list" id="coffee-list">
+        <option value={null}>Select Type</option>
+        {coffeeOptions.map((option, index) => {
+          return (
+            <option value={option.name} key={index}>
+              {option.name} ({option.caffeine} mg)
+            </option>
+          );
+        })}
+      </select>
     </>
   );
 }
