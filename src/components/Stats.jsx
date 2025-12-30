deb2242faa30bd1cdcf797184766e1206aeedc50
WJijon223 <williamjijon223@gmail.com>
2025-12-30 16:44:31 -0500

    added mock info for active caffeine level stats (will update later)

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,10 +1,30 @@
+import {
+  calculateCurrentCaffeineLevel,
+  coffeeConsumptionHistory,
+  statusLevels,
+} from "../utils";
+
 function StatCard(props) {
   const { lg, title, children } = props;
 
-  return <div className={"card stat-card " + (lg ? "col-span-2" : "")}></div>;
+  return (
+    <div className={"card stat-card " + (lg ? "col-span-2" : "")}>
+      <h4>{title}</h4>
+      {children}
+    </div>
+  );
 }
 
 export default function Stats() {
+  const stats = {
+    daily_caffeine: 240,
+    daily_cost: 6.8,
+    average_coffees: 2.3,
+    total_cost: 220,
+  };
+
+  const caffeineLevel = calculateCurrentCaffeineLevel(coffeeConsumptionHistory);
+
   return (
     <>
       <div className="section-header">
@@ -12,11 +32,30 @@ export default function Stats() {
         <h2>Stats</h2>
       </div>
       <div className="stats-grid">
-        <StatCard lg></StatCard>
-        <StatCard></StatCard>
-        <StatCard></StatCard>
-        <StatCard></StatCard>
-        <StatCard></StatCard>
+        <StatCard lg title="Active Caffeine Level">
+          <div className="status">
+            <p>
+              <span className="stat-text">{caffeineLevel}</span>mg
+            </p>
+            <h5
+              style={{
+                color: statusLevels["low"].color,
+                background: statusLevels["low"].background,
+              }}
+            >
+              Low
+            </h5>
+          </div>
+          <p>{statusLevels["low"].description}</p>
+        </StatCard>
+        <StatCard title="Daily Caffeine">
+          <p>
+            <span></span>
+          </p>
+        </StatCard>
+        <StatCard title="Avg # of Coffees"></StatCard>
+        <StatCard title="Daily Cost ($)"></StatCard>
+        <StatCard title="Total Cost ($)"></StatCard>
       </div>
     </>
   );
