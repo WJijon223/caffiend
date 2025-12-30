26203b5c9dbff32a1364d607f164a7f4a83f88f6
WJijon223 <williamjijon223@gmail.com>
2025-12-30 16:25:04 -0500

    started grid layout of StatCard component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import Layout from "./components/Layout";
 import CoffeeForm from "./components/CoffeeForm";
 import Hero from "./components/Hero";
-import Stats from "./components/History";
+import Stats from "./components/Stats";
 import History from "./components/History";
 
 function App() {
-  const isAuthenticated = false;
+  const isAuthenticated = true;
 
   const authenticatedContent = (
     <>
