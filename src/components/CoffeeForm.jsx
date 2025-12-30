f5fc4356572a3dd704971ce814796359798762d8
WJijon223 <williamjijon223@gmail.com>
2025-12-30 16:16:54 -0500

    added functionality to coffeeForm

--- src/components/CoffeeForm.jsx
+++ src/components/CoffeeForm.jsx
@@ -1,6 +1,17 @@
 import { coffeeOptions } from "../utils";
+import { use, useState } from "react";
 
 export default function CoffeeForm() {
+  const [selectedCoffee, setSelectedCoffee] = useState(null);
+  const [showCoffeeTypes, setShowCoffeeTypes] = useState(false);
+  const [coffeeCost, setCoffeeCost] = useState(0);
+  const [hour, setHour] = useState(0);
+  const [min, setMin] = useState(0);
+
+  function handleSubmitForm() {
+    console.log(selectedCoffee, coffeeCost, hour, min);
+  }
+
   return (
     <>
       <div className="section-header">
@@ -11,34 +22,73 @@ export default function CoffeeForm() {
       <div className="coffee-grid">
         {coffeeOptions.slice(0, 5).map((option, index) => {
           return (
-            <button className="button-card" key={index}>
+            <button
+              onClick={() => {
+                setSelectedCoffee(option.name);
+                setShowCoffeeTypes(false);
+              }}
+              className={
+                "button-card " +
+                (selectedCoffee === option.name ? "coffee-button-selected" : "")
+              }
+              key={index}
+            >
               <h4>{option.name}</h4>
               <p>{option.caffeine}mg</p>
             </button>
           );
         })}
-        <button className="button-card">
+        <button
+          onClick={() => {
+            setShowCoffeeTypes(true);
+            setSelectedCoffee(null);
+          }}
+          className={
+            "button-card " + (showCoffeeTypes ? "coffee-button-selected" : "")
+          }
+        >
           <h4>Other</h4>
           <p>N/A</p>
         </button>
       </div>
-      <select name="coffee-list" id="coffee-list">
-        <option value={null}>Select Type</option>
-        {coffeeOptions.map((option, index) => {
-          return (
-            <option value={option.name} key={index}>
-              {option.name} ({option.caffeine} mg)
-            </option>
-          );
-        })}
-      </select>
+      {showCoffeeTypes && (
+        <select
+          onChange={(event) => {
+            setSelectedCoffee(event.target.value);
+          }}
+          name="coffee-list"
+          id="coffee-list"
+        >
+          <option value={null}>Select Type</option>
+          {coffeeOptions.map((option, index) => {
+            return (
+              <option value={option.name} key={index}>
+                {option.name} ({option.caffeine} mg)
+              </option>
+            );
+          })}
+        </select>
+      )}
       <h4>Add the cost ($)</h4>
-      <input className="w-full" type="number" placeholder="4.50" />
+      <input
+        className="w-full"
+        type="number"
+        value={coffeeCost}
+        onChange={(event) => {
+          setCoffeeCost(event.target.value);
+        }}
+        placeholder="4.50"
+      />
       <h4>Time since consumption</h4>
       <div className="time-entry">
         <div>
           <h6>Hours</h6>
-          <select id="hours-select">
+          <select
+            id="hours-select"
+            onChange={(event) => {
+              setHour(event.target.value);
+            }}
+          >
             {[
               0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18,
               19, 20, 21, 22, 23,
@@ -53,7 +103,12 @@ export default function CoffeeForm() {
         </div>
         <div>
           <h6>Minutes</h6>
-          <select id="mins-select">
+          <select
+            id="mins-select"
+            onChange={(event) => {
+              setMin(event.target.value);
+            }}
+          >
             {[0, 5, 10, 15, 30, 45].map((min, index) => {
               return (
                 <option key={index} value={min}>
@@ -64,7 +119,7 @@ export default function CoffeeForm() {
           </select>
         </div>
       </div>
-      <button>
+      <button onClick={handleSubmitForm}>
         <p>Add Entry</p>
       </button>
     </>
