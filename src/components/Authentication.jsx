fb2c7dae039c05ee51d6aef6c67d51db35fbf209
WJijon223 <williamjijon223@gmail.com>
2026-01-12 02:16:19 -0500

    built out modal and implemented modal functions

--- src/components/Authentication.jsx
+++ src/components/Authentication.jsx
@@ -1,3 +1,20 @@
 export default function Authentication() {
-  return <></>;
+  return (
+    <>
+      <h2 className="sign-up-text">Sign Up / Login</h2>
+      <p>Sign into your account!</p>
+      <input placeholder="Email" />
+      <input placeholder="*********" type="password" />
+      <button>
+        <p>Submit</p>
+      </button>
+      <hr />
+      <div className="register-content">
+        <p>Don&apos;t have an account?</p>
+        <button>
+          <p>Sign up</p>
+        </button>
+      </div>
+    </>
+  );
 }
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,5 +1,11 @@
+import { useState } from "react";
+import Modal from "./Modal";
+import Authentication from "./Authentication";
+
 export default function Layout(props) {
   const { children } = props;
+
+  const [showModal, setShowModal] = useState(false);
 
   const header = (
     <header>
@@ -7,7 +13,11 @@ export default function Layout(props) {
         <h1 className="text-gradient">CAFFIEND</h1>
         <p>For Coffee Insatiates</p>
       </div>
-      <button>
+      <button
+        onClick={() => {
+          setShowModal(true);
+        }}
+      >
         <p>Sign up free</p>
         <i className="fa-solid fa-mug-hot"></i>
       </button>
@@ -21,13 +31,31 @@ export default function Layout(props) {
         <a href="https://www.fantacss.smoljames.com/" target="_blank">
           FantaCSS
         </a>{" "}
-        design library
+        design library.
+        <br />
+        Check out the project on{" "}
+        <a
+          target="_blank"
+          href="https://github.com/jamezmca/reactjs-full-course"
+        >
+          Github
+        </a>
+        !
       </p>
     </footer>
   );
 
   return (
     <>
+      {showModal && (
+        <Modal
+          handleCloseModal={() => {
+            setShowModal(false);
+          }}
+        >
+          <Authentication />
+        </Modal>
+      )}
       {header}
       <main>{children}</main>
       {footer}
